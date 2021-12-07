65907878971da80cd87d65b4b7f662d680108ab9
Igor <aksel-zig@mail.ru>
2021-12-07 18:13:09 +0700

    homework done

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import {
   h,
 } from "snabbdom";
 
+import './assets/style.css';
+
 const patch = init([
   classModule,
   propsModule,
@@ -16,6 +18,56 @@ const patch = init([
 
 const app = document.getElementById("app");
 
-const vnode = h("h1", null, "Hello");
+const userList = [
+  {
+    imgPath: './src/assets/img/1.jpg',
+    username: 'Lorem',
+    descriotion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+  },
+  {
+    imgPath: './src/assets/img/2.jpg',
+    username: 'Dolorem',
+    descriotion: 'Dolorem at nesciunt adipisci rem.'
+  },
+  {
+    imgPath: './src/assets/img/3.jpg',
+    username: 'Numquam',
+    descriotion: 'Numquam, vel.'
+  },
+  {
+    imgPath: './src/assets/img/4.jpg',
+    username: 'Porro',
+    descriotion: 'Porro eligendi reiciendis voluptatibus dolor temporibus natus et, vero tempore est.'
+  },
+  {
+    imgPath: './src/assets/img/5.jpg',
+    username: 'Repellendus',
+    descriotion: 'Repellendus ut eos natus voluptates magni fugit, ipsam fugiat error deleniti distinctio rem eveniet eligendi rerum minima hic nostrum nihil dolorum sunt aspernatur adipisci!'
+  },
+  {
+    imgPath: './src/assets/img/6.jpg',
+    username: 'Voluptatem',
+    descriotion: 'Voluptatem delectus beatae ullam quia voluptate quasi ipsa.'
+  }
+]
+
+const usersNode = userList.reduce((acc, item) => {
+  const currentUser = [
+    h('article.userList__itemContainer', null, [
+      h('img.userList__image', {props: {
+        src: item.imgPath
+      }}),
+      h('div.userList__description', null, [
+        h('h3.userList__username', item.username),
+        h('span', item.descriotion)
+      ])
+    ])
+  ]
+  acc = acc.concat(currentUser)
+  return acc
+}, [] )
+
+
+const vnode = h("section.userList", null, usersNode);
 
 patch(app, vnode);
