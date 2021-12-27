6361fd68db9025cbf1f951e0fc442c358f33793f
Igor Tsoy <aksel-zig@mail.ru>
2021-12-28 01:14:04 +0700

    add arr.map, add img to build

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import {
 } from "snabbdom";
 
 import './assets/style.css';
+import './assets/img/1.jpg'
+import './assets/img/2.jpg'
+import './assets/img/3.jpg'
+import './assets/img/4.jpg'
+import './assets/img/5.jpg'
+import './assets/img/6.jpg'
 
 const patch = init([
   classModule,
@@ -22,51 +28,51 @@ const userList = [
   {
     imgPath: './src/assets/img/1.jpg',
     username: 'Lorem',
-    descriotion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
   },
   {
     imgPath: './src/assets/img/2.jpg',
     username: 'Dolorem',
-    descriotion: 'Dolorem at nesciunt adipisci rem.'
+    description: 'Dolorem at nesciunt adipisci rem.'
   },
   {
     imgPath: './src/assets/img/3.jpg',
     username: 'Numquam',
-    descriotion: 'Numquam, vel.'
+    description: 'Numquam, vel.'
   },
   {
     imgPath: './src/assets/img/4.jpg',
     username: 'Porro',
-    descriotion: 'Porro eligendi reiciendis voluptatibus dolor temporibus natus et, vero tempore est.'
+    description: 'Porro eligendi reiciendis voluptatibus dolor temporibus natus et, vero tempore est.'
   },
   {
     imgPath: './src/assets/img/5.jpg',
     username: 'Repellendus',
-    descriotion: 'Repellendus ut eos natus voluptates magni fugit, ipsam fugiat error deleniti distinctio rem eveniet eligendi rerum minima hic nostrum nihil dolorum sunt aspernatur adipisci!'
+    description: 'Repellendus ut eos natus voluptates magni fugit, ipsam fugiat error deleniti distinctio rem eveniet eligendi rerum minima hic nostrum nihil dolorum sunt aspernatur adipisci!'
   },
   {
     imgPath: './src/assets/img/6.jpg',
     username: 'Voluptatem',
-    descriotion: 'Voluptatem delectus beatae ullam quia voluptate quasi ipsa.'
+    description: 'Voluptatem delectus beatae ullam quia voluptate quasi ipsa.'
   }
 ]
 
-const usersNode = userList.reduce((acc, item) => {
-  const currentUser = [
+
+
+const usersNode = userList.map((currentUser) => {
+  const userNode = [
     h('article.userList__itemContainer', null, [
       h('img.userList__image', {props: {
-        src: item.imgPath
+        src: currentUser.imgPath
       }}),
       h('div.userList__description', null, [
-        h('h3.userList__username', item.username),
-        h('span', item.descriotion)
+        h('h3.userList__username', currentUser.username),
+        h('span', currentUser.description)
       ])
     ])
   ]
-  acc = acc.concat(currentUser)
-  return acc
-}, [] )
-
+  return userNode[0]
+})
 
 const vnode = h("section.userList", null, usersNode);
 
